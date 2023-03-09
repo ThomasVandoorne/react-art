af6121ca1cb3fbccbbb57511d5c645200ddddd6c
Your Name Here <your_email@student.howest.be>
2023-03-09 21:03:27 +0100

    test

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Slider from "./components/Slider";
+import Drawing from "./components/Drawing";
+
+let x = Math.floor(Math.random() * 1);
+
+const red = { backgroundColor: "red" };
+const blue = { backgroundColor: "blue" };
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [radius, setRadius] = useState(10);
+  const [color, setColor] = useState(red);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={color} className="App">
+      <Slider
+        max={10}
+        label="Radius"
+        value={radius}
+        onValueChange={(v) => setRadius(v)}
+      />
+      <Drawing radius={radius} />
+      <button onClick={() => setColor(blue)}>blue</button>
+      <button onClick={() => setColor(red)}>red</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
