79e617716c8e2f87564d2af65d957246ad0775bd
Your Name Here <your_email@student.howest.be>
2023-03-15 09:33:28 +0100

    deploy

--- vite-project/src/components/Drawing.jsx
+++ vite-project/src/components/Drawing.jsx
@@ -33,6 +33,13 @@ const Drawing = ({ radius, strokeWidth, red, green, blue }) => {
         cy="50"
         r={radius}
       />
+      {/* Hair */}
+
+      <circle fill="green" cx="45" cy="35" r="10" />
+      <circle fill="green" cx="60" cy="30" r="10" />
+      <circle fill="green" cx="65" cy="40" r="8" />
+      <circle fill="green" cx="30" cy="40" r="7.5" />
+
       <circle fill={`rgb(${red},${green}, ${blue})`} cx="50" cy="60" r="7.5" />
     </svg>
   );
